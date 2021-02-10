054b399dceb524da6e11dab71857b87c366af205
PepperMcLean <jackmcl206@gmail.com>
2021-02-10 12:37:23 -0600

    commit tree f4cb43aa6ddf5724d517ab103d1997d2ee17b1dd

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+katz_deli = []
+def line(array) 
+    if array.length() == 0
+        puts "The line is currently empty."
+    else 
+        newArray = []
+        array.each_with_index do |name, index|
+            newArray << "#{index+1}."
+            newArray << name 
+        end
+        puts "The line is currently: #{newArray.join(" ")}"
+    end            
+end
+
+
+def take_a_number(array, name)
+    array.push(name)
+    puts "Welcome, #{name}. You are number #{array.length()} in line."
+end
+
+
+def now_serving(array)
+    if array.length() == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array[0]}."
+        array.shift()
+    end
+end
